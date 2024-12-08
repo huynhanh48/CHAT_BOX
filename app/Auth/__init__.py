2e47ddbe784ea40e0fae6e333bb4a73979091287
anhvo <anhvo482004@gmail.com>
2024-12-08 11:42:34 +0700

    login-notifications

--- app/Auth/__init__.py
+++ app/Auth/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, flash, render_template, redirect, url_for
-from .form import Register
+from .form import Register,Login
 from ..moduls import User
 from ..db_conection import db
+from flask_login import login_user,logout_user,login_required
 
 auth_pages = Blueprint('auth_pages', __name__)
 
@@ -19,10 +20,57 @@ def register():
             flash('user name already', 'danger')
             return render_template('Auth/register.html', form=form)
 
-        # Tạo mới người dùng
+       
         employ = User(email=form.email.data, user_name=form.user_name.data, password=form.password.data)
         db.session.add(employ)
         db.session.commit()
         flash('register success', 'success')
-        return redirect(url_for('auth_pages.login'))  # Redirect to login page
+        return redirect(url_for('auth_pages.login'))  
     return render_template('Auth/register.html', form=form)
+
+# @auth_pages.route('/login',methods=['GET','POST'])
+# def login():
+#     form =Login()
+#     if(form.validate_on_submit()):
+#         user = User.query.filter_by(email=form.email.data).first()
+#         print(user)
+#         if(not user):
+#             flash('invalid email','danger')
+#             return  render_template('Auth/login.html',form = form)
+#         elif (user.verify_password(form.password.data)):
+#             flash('login success','success')
+#             login_user(user)
+#             return  redirect(url_for('home_page.home'))
+#         else:
+#             flash('please  check your login  and try again.','danger')
+#             return  render_template('Auth/login.html',form = form)
+#     return   render_template('Auth/login.html',form = form)
+@auth_pages.route('/login', methods=['GET', 'POST'])
+def login():
+    form = Login()
+    if form.validate_on_submit():
+        print("Form is valid")  # Check if form is valid
+        print("Email entered:", form.email.data)  # Print email entered in the form
+        user = User.query.filter_by(email=form.email.data).first()
+        print(user)  # This should print the user object if found, or None if not found
+        
+        if not user:
+            flash('Invalid email', 'danger')
+            return render_template('Auth/login.html', form=form)
+        elif user.verify_password(form.password.data):
+            login_user(user)
+            return redirect(url_for('home_page.home'))
+        else:
+            flash('Please check your login and try again.', 'danger')
+            return render_template('Auth/login.html', form=form)
+    else:
+        print("Form is not valid")  # Form validation failed
+        print(form.errors)  # Print any form validation errors
+
+    return render_template('Auth/login.html', form=form)
+
+
+@auth_pages.route('/logout')
+def logout():
+    logout_user()  
+    return redirect(url_for('home_page.home'))
--- app/Auth/form.py
+++ app/Auth/form.py
@@ -9,20 +9,20 @@ class Register(FlaskForm):
     confirm_password=PasswordField('confirm_password')
     submit = SubmitField('register')
 
-    def validate_email(self,field):
-        if(User.query.filter_by(email=field.data).first()):
-            raise  ValidationError('email already')
+    # def validate_email(self,field):
+    #     if(User.query.filter_by(email=field.data).first()):
+    #         raise  ValidationError('email already')
         
-    def validate_username(self,field):
-        if(User.query.filter_by(user_name=field.data).first()):
-            raise ValidationError('user name already')
+    # def validate_username(self,field):
+    #     if(User.query.filter_by(user_name=field.data).first()):
+    #         raise ValidationError('user name already')
         
 class Login(FlaskForm):
     email= StringField('email',validators=[DataRequired(),Email()])
     password = PasswordField('password',validators=[DataRequired()])
     submit = SubmitField('login')
     
-    def validate_email(self,field):
-        user =User.query.filter_by(email=field.data).first()
-        if(not user):
-            raise  ValidationError('email does not exist')
+    # def validate_email(self,field):
+    #     user =User.query.filter_by(email=field.data).first()
+    #     if(not user):
+    #         raise  ValidationError('email does not exist')
